89b7a8a9ef1a20d82b259fbf703c5be467df9492
Aristyo RS <aristyo2006@gmail.com>
2026-04-26 03:50:00 +0800

    ts pmo

--- resources/views/partials/favicons.blade.php
+++ resources/views/partials/favicons.blade.php
@@ -1,2 +1,3 @@
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
 <link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}">
